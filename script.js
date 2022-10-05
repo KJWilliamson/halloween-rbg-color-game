166f0b447ddf3b884ddc476f6e22c7fae7bfbff3
kamela williamson <kami63116@yahoo.com>
2022-10-04 18:28:58 -0700

    removed any javascript. starting from scratch to figure out functionality

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// stores the number of squares that will be available in the game as per the mode
-let numOfSquares = 6;
-
-// empty array which contains the random six or three RGB color code generated every time the game is reset or the mode is changed.
-let colors = [];
-
-let pickedColor;
-let squares = document.querySelectorAll(".square");
-let resetButton = document.querySelector("#reset");
-let modeButtons = document.querySelectorAll(".mode");
+// Guess what the RGB color is from the colors displayed
 
